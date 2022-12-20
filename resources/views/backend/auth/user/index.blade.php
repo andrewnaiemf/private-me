0062abe48c7dd7fb8b4c1a902d8605f44ac2998e
andrew <andrewnaief@gmail.com>
2022-12-20 03:34:00 +0200

    edit user id in users page

--- resources/views/backend/auth/user/index.blade.php
+++ resources/views/backend/auth/user/index.blade.php
@@ -42,7 +42,7 @@
                             @isset($users)
                                 @foreach ($users as $user)
                                     <tr>
-                                        <td>{{$user->id + 10000}}</td>
+                                        <td>{{$user->id + 100000}}</td>
                                         <td>{{$user->first_name . ' '.$user->last_name}}</td>
                                         <td>{{$user->email}}</td>
                                         {{--  <td>{{$user->confirmed == 1 ? 'yes':'no'}}</td>  --}}
